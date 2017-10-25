8ad3e62176451cb1c9ad0d45f69042f851f90bb1
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-10-25 16:28:18 -0600

    Fix a production bug by listening on a port
    Previously, when pushed to production on Heroku, the application would crash because the server was not listening on a port. This commit installs Express and uses it to listen on either the Node process port (provided by Heroku in production), or otherwise port 3000 for develoment.

--- server.js
+++ server.js
@@ -2,7 +2,14 @@
 
 const apiUrl = require('./config/apiUrl');
 
+const express = require('express');
 const WebSocket = require('ws');
+const path = require('path');
+
+const PORT = process.env.PORT || 3000;
+
+const server = express()
+  .listen(PORT, () => console.log(`Listening on port ${PORT}`))
 
 const webSocketClient = new WebSocket(apiUrl, {
   perMessageDeflate: false
