5a13ee93be289cce6f42ae6521398d170df3c39d
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-10-31 10:28:42 -0600

    Add a test for inserting a trade into the database
    When a trade is inserted into the database, a Promise should be returned whose resolve value is the newly created document. Also, the count of documents in the database should increase by one.

--- test/trade_test.js
+++ test/trade_test.js
@@ -1,5 +1,8 @@
 const mongodb = require('mongodb');
 const mongoDbUri = require('../secrets/testDatabaseUri');
+const { expect } = require('chai');
+const Trade = require('../models/Trade');
+const TRADES_COLLECTION = 'trades';
 
 describe('Interacting with the trades database', () => {
   let db;
@@ -15,7 +18,22 @@ describe('Interacting with the trades database', () => {
     });
   });
 
-  // it(' ', () => {
-  //
-  // });
+  it('inserts a trade into the database', (done) => {
+    const trade = { tradePrice: 6323.7, timeStamp: 1509458089 };
+    db.collection(TRADES_COLLECTION).count()
+    .then((count) => {
+      const initialCount = count;
+      Trade.insert(trade, db)
+      .then((newTrade) => {
+        expect(newTrade.tradePrice).to.equal(6323.7);
+        expect(newTrade.timeStamp).to.equal(1509458089);
+        db.collection(TRADES_COLLECTION).count()
+        .then((count) => {
+          const afterCount = count;
+          expect(afterCount).to.eql(initialCount + 1)
+          done();
+        });
+      });
+    });
+  });
 });
