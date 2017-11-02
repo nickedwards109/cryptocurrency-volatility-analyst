3d03c30f468f5207d74bb9ef23f74f873ee15822
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-11-02 12:07:37 -0600

    Add a test for calculating the rate of return
    This commit adds a unit test for a method Trade.rateOfReturn, which should take an object containing an initial trade and a final trade. This method should calculate the instantaneous rate of return between those two trades, defined as the percentage change in US Dollar price per second.

--- test/trade_test.js
+++ test/trade_test.js
@@ -39,4 +39,27 @@ describe('Interacting with the trades database', () => {
       });
     });
   });
+
+  it('calculates the instantaneous rate of return', () => {
+    // This rate is defined as the percentage change in US Dollars per second.
+    // Test for a positive rate of return.
+    // In the following test case, the price increases by five percent over ten seconds,
+    // resulting in a rate of .05 / 10 = 0.005, or 0.5 % per second.
+    const positiveReturnInitialTrade = { price: 100, timeStamp: 1500000000 };
+    const positiveReturnFinalTrade = { price: 105, timeStamp: 1500000010 };
+    const positiveReturn = Trade.rateOfReturn({
+      initialTrade: positiveReturnInitialTrade,
+      finalTrade: positiveReturnFinalTrade
+    });
+    expect(positiveReturn).to.equal(0.005);
+
+    // Test for a negative rate of return.
+    const negativeReturnInitialTrade = { price: 100, timeStamp: 1500000000 };
+    const negativeReturnFinalTrade = { price: 95, timeStamp: 1500000010 };
+    const negativeReturn = Trade.rateOfReturn({
+      initialTrade: negativeReturnInitialTrade,
+      finalTrade: negativeReturnFinalTrade
+    });
+    expect(negativeReturn).to.equal(-0.005);
+  });
 });
