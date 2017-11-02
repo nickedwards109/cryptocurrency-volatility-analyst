d01e93340b527d2bb561fba8a5ef1d2a9d3fc575
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-11-02 17:05:14 -0600

    Implement inserting a rate of return into the database
    This commit implements the ability to insert a rate of return into the database. When RateOfReturn.insert(rateOfReturn, db) is executed, it returns a Promise that will either resolve with a document that was inserted into the database, or reject with an error message.

--- models/RateOfReturn.js
+++ models/RateOfReturn.js
@@ -1,3 +1,5 @@
+const RATES_OF_RETURN_COLLECTION = require('../config/db_collections').ratesOfReturn;
+
 class RateOfReturn {
   constructor(tradePair) {
     const initialPrice = tradePair.initialTrade.price;
@@ -9,6 +11,23 @@ class RateOfReturn {
     const intervalSeconds = finalTimeStamp - initialTimeStamp;
 
     this.rate = (priceChangePercentage / intervalSeconds);
+  }
+
+  static insert(rateOfReturn, db) {
+    return new Promise((resolve, reject) => {
+      db.collection(RATES_OF_RETURN_COLLECTION).insertOne(rateOfReturn, (err, doc) => {
+        if (err) {
+          console.log('There was an error inserting the trade into the database.');
+          console.log(err)
+          reject(err);
+        } else {
+          const createdRateOfReturn = doc.ops[0];
+          console.log('Inserted a rate of return into the database.');
+          console.log(createdRateOfReturn);
+          resolve(createdRateOfReturn);
+        }
+      });
+    });
   }
 }
 
