d6c597ed92140f4d159dbd1113d6caac46fea767
edwardsCNC <nicholas.lee.edwards@gmail.com>
2017-11-04 12:13:29 -0600

    Add a test for getting all returns in a trailing time period
    This commit adds a test for the method RateOfReturn.getTrailing(), which takes an object with the keys 'hours' and 'db' (representing the number of hours in the trailing time period, and the database connection instance). This method should return an Array containing all of the rate of return objects in the database within the specified trailing number of hours.

--- test/rate_of_return_test.js
+++ test/rate_of_return_test.js
@@ -2,6 +2,7 @@ const mongodb = require('mongodb');
 const mongoDbUri = require('../secrets/testDatabaseUri');
 const { expect } = require('chai');
 const RateOfReturn = require('../models/RateOfReturn');
+const Trade = require('../models/Trade');
 const RATES_OF_RETURN_COLLECTION = require('../config/db_collections').ratesOfReturn;
 
 describe('Rates of return', () => {
@@ -68,4 +69,56 @@ describe('Interacting with the rate of return database', () => {
       });
     });
   });
-})
+
+  it('gets the trailing 24 hours of rate of return data', (done) => {
+    // Set up three times: one right now, one within the trailing time period,
+    // and one outside of the trailing time period
+    const nowTime = Math.floor(Date.now() / 1000);
+    const recentTime = nowTime - 100;
+    const staleTime = nowTime - 60*60*24 - 1;
+
+    // Calculate a rate of return where the timestamp is now
+    const nowTrade1 = new Trade({price: 100, timeStamp: nowTime - 10});
+    const nowTrade2 = new Trade({price: 105, timeStamp: nowTime});
+    const nowRateOfReturn = new RateOfReturn({
+      initialTrade: nowTrade1,
+      finalTrade: nowTrade2
+    });
+
+    // Calculate a rate of return within the trailing time period
+    const recentTrade1 = new Trade({price: 100, timeStamp: recentTime - 10});
+    const recentTrade2 = new Trade({price: 105, timeStamp: recentTime});
+    const recentRateOfReturn = new RateOfReturn({
+      initialTrade: recentTrade1,
+      finalTrade: recentTrade2
+    });
+
+    // Calculate a rate of return outside the trailing time period
+    const staleTrade1 = new Trade({price: 200, timeStamp: staleTime});
+    const staleTrade2 = new Trade({price: 205, timeStamp: staleTime - 10});
+    const staleRateOfReturn = new RateOfReturn({
+      initialTrade: staleTrade1,
+      finalTrade: staleTrade2
+    });
+
+    // Insert all three rates of return in the database
+    // Expect the trailing time period to only have two rates of return
+    RateOfReturn.insert(staleRateOfReturn, db)
+    .then(() => {
+      return RateOfReturn.insert(recentRateOfReturn, db);
+    })
+    .then(() => {
+      return RateOfReturn.insert(nowRateOfReturn, db);
+    })
+    .then(() => {
+      return RateOfReturn.getTrailing({ hours: 24, db: db});
+    })
+    .then((trailingRatesOfReturn) => {
+      expect(typeof trailingRatesOfReturn).to.equal('object');
+      expect(trailingRatesOfReturn.length).to.equal(2);
+      expect(trailingRatesOfReturn[0].rate).to.equal(0.005);
+      expect(trailingRatesOfReturn[1].rate).to.equal(0.005);
+      done();
+    });
+  });
+});
